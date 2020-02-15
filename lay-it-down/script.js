12bc097e418d35460ba1a5f199a3b1dea1f09a8d
Marco A. Siurob <sicma795@gmail.com>
2020-02-14 21:38:58 -0600

    Recover page structure

--- lay-it-down/script.js
+++ lay-it-down/script.js
@@ -1,11 +1,15 @@
-const princess = { };
+const scene = { };
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
 
-  princess.img = loadImage('img/princess.jpeg');
+  scene.princess = loadImage('img/princess.jpeg');
+  scene.counter = 0;
+  scene.particleSystem = new ParticleSystem(-10, 0);
   
   colorMode(HSB);
+
+  rectMode(CENTER);
 
   imageMode(CENTER);
 
@@ -17,13 +21,44 @@ function setup() {
 }
 
 function draw() {
+  scene.counter ++;
+  if(scene.counter > 360) { scene.counter = 0; }
+
+  background(0);
+
+  push();
+
+  scene.particleSystem.insertParticle(scene.counter);
+  scene.particleSystem.update();
+  scene.particleSystem.display();
+
+  pop();
+
   displayImage();
+  displayText(scene.counter);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 const displayImage = () => {
   push();
   translate(width / 2, height / 2);
-  scale(0.2);
-  image(princess.img, 0, 0);
+  scale(0.5);
+  strokeWeight(30);
+  stroke(60, 100, 100);
+  rect(-1, -1, scene.princess.width, scene.princess.height);
+  image(scene.princess, 0, 0);
+  pop();
+}
+
+const displayText = c => {
+  push();
+  translate(width / 2, 7 * height / 8);
+  stroke(0, 0, 100);
+  fill(c, 100, 90);
+  text('Karlita mi princesa preciosa', 0, 0);
+  text('TE AMO \u2764', 0, 30);
   pop();
 }
